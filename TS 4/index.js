3756beb01fc7162dd132d4551d0f70384199c92f
kartik0603 <kartikhirapara800@gmail.com>
2024-04-17 19:45:10 +0530

    js_task_4

---
+++ TS 4/index.js
@@ -0,0 +1,14 @@
+let one = 2;
+let two = 5;
+let three = 8;
+
+let squareOne = one * one;
+let squareTwo = two * two;
+let squareThree = three * three;
+
+let sum = squareOne + squareTwo + squareThree;
+
+console.log(`Square of One is ${squareOne}`);
+console.log(`Square of Two is ${squareTwo}`);
+console.log(`Square of Three is ${squareThree}`);
+console.log(`Sum of squares is ${sum}`);
