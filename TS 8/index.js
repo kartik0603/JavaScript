dca3816012fb16617665fac440728f242c8c1e7d
kartik0603 <kartikhirapara800@gmail.com>
2024-04-17 20:04:42 +0530

    js_task_8

---
+++ TS 8/index.js
@@ -0,0 +1,11 @@
+let first=10;
+let second = 5;
+let third = 3;
+
+let sum = first + second;
+let divisionResult = sum / third;
+let remainder = sum % third;
+
+console.log(`Sum of firstVariable and secondVariable: ${sum}`);
+console.log(`Division result: ${divisionResult}`);
+console.log(`Remainder of the division: ${remainder}`);
