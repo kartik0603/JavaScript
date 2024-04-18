e8d60252544cfc6626a24c6b15259a95fcc89dda
kartik0603 <kartikhirapara800@gmail.com>
2024-04-18 18:39:43 +0530

    js_task_17

---
+++ TS 17/index.js
@@ -0,0 +1,12 @@
+
+let num1 = 10;
+let num2 = 35;
+
+
+if (num1 > num2) {
+    console.log("num1 is the largest:", num1);
+} else if (num1 < num2) {
+    console.log("num2 is the largest:", num2);
+} else {
+    console.log("num1 and num2 are equal:", num1);
+}
