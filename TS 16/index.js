b45fbc0b4e2503b02bf896076f91e936d3da9838
kartik0603 <kartikhirapara800@gmail.com>
2024-04-18 18:37:21 +0530

    js_task_16

---
+++ TS 16/index.js
@@ -0,0 +1,12 @@
+
+let num1 = 20;
+let num2 = 25;
+let num3 = 10;
+
+if (num1 > num2 && num1 > num3) {
+  console.log("num1 is the largest:", num1);
+} else if (num2 > num1 && num2 > num3) {
+  console.log("num2 is the largest:", num2);
+} else {
+  console.log("num3 is the largest:", num3);
+}
