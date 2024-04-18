ba5df545927339a75d373100f1e527df2c2fff25
kartik0603 <kartikhirapara800@gmail.com>
2024-04-18 19:03:17 +0530

    js_task_22

---
+++ TS 22/index.js
@@ -0,0 +1,11 @@
+
+let Australia = 46; 
+let England = 67; 
+
+if (Australia > England) {
+    console.log("Australia");
+} else if (Australia < England) {
+    console.log("England");
+} else if (Australia === England) {
+    console.log("Tie");
+}
