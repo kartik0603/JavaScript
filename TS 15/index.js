aa2d9c1e61555553a5212a4b36ccfba7d9de60b4
kartik0603 <kartikhirapara800@gmail.com>
2024-04-18 18:34:16 +0530

    js_task_15

---
+++ TS 15/index.js
@@ -0,0 +1,17 @@
+
+let age = 20; 
+
+
+let N = 15; 
+
+
+
+if (N < 13) {
+    console.log("1 Kms");
+} else if (age >= 13 && age < 18) {
+    console.log("5 Kms");
+} else if (age >= 18 && age < 30) {
+    console.log("10 Kms");
+} else {
+    console.log("You can have friends from anywhere");
+}
