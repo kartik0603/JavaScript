3aaab700aab608e96600f6a23071060fd08e7411
kartik0603 <kartikhirapara800@gmail.com>
2024-04-24 10:47:31 +0530

    js_task_29

---
+++ TS 29/index.js
@@ -0,0 +1,10 @@
+const GetValue =()=>{
+    let num1 =document.getElementById("num1").value;
+    let num2 =document.getElementById("num2").value;
+    
+
+    document.getElementById("result1").innerHTML= Number(num1);
+    document.getElementById("result2").innerHTML= Number(num2);
+
+
+}
