1c2378d8288f857318b4df8ae318a5c2e7c76e3b
kartik0603 <kartikhirapara800@gmail.com>
2024-04-24 10:54:53 +0530

    js_task_31

---
+++ TS 31/index.js
@@ -0,0 +1,10 @@
+const GetValue =()=>{
+    let num1 =document.getElementById("num1").value;
+    let num2 =document.getElementById("num2").value;
+    let num3 =document.getElementById("num3").value;
+    let num4 =document.getElementById("num4").value;
+  
+
+    document.getElementById("result").innerHTML=  Number(num1)-  Number(num2)- Number(num3)- Number(num4);
+
+}
