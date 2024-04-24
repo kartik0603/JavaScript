ba2b32dc648e76645facd3feafaffb86b342bb93
kartik0603 <kartikhirapara800@gmail.com>
2024-04-24 10:33:08 +0530

    js_task_27

---
+++ TS 27/index.js
@@ -0,0 +1,13 @@
+const GetValue =()=>{
+    let Name =document.getElementById("Name").value;
+    let Age =document.getElementById("Age").value;
+
+    document.getElementById("result1").innerHTML=(`Name:-${Name} `);
+    document.getElementById("result2").innerHTML=(`Age:-${Age}`);
+
+
+    
+
+    
+   
+}
