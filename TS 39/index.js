4d64d0d8d00f5b3d769279f53760ac4b54546a14
kartik0603 <kartikhirapara800@gmail.com>
2024-05-01 11:43:32 +0530

    js_task_39

---
+++ TS 39/index.js
@@ -0,0 +1,10 @@
+let array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+let max = 0;
+
+for (let i = 0; i <= array.length; i++) {
+  if (array[i] > max) {
+    max = array[i];
+  }
+}
+console.log(max);
+
