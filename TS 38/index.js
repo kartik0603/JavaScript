034e82d145e8bea584bdcc8916087d7d2df60644
kartik0603 <kartikhirapara800@gmail.com>
2024-05-01 11:38:40 +0530

    js_task_38

---
+++ TS 38/index.js
@@ -0,0 +1,7 @@
+let array = ["RED", "AND", "WHITE"];
+let arr0 = array[0].toUpperCase();
+let arr1 = array[1].toLowerCase();
+let arr2 = array[2].toUpperCase();
+
+let newArray = [arr0, arr1, arr2].join(' ');
+console.log(newArray);
